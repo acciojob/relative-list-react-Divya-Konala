cd3c87f3a8d355f36f733d7e3069a537e9b2c64c
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-03 16:36:08 +0530

    relative-list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+const relatives=["Amma","Nanna","Ammamma","Tata","Nanamma","Tata",
+"Peddamma","Attha"];
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={'relativeList'}>
+                  {relatives.map((item,index)=>{
+                    return <li key={'relativeListItem'+(index+1)}>
+                      {item}
+                    </li>
+                  })}
+               </ol>
     </div>
   )
 }
